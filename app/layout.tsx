0b49dd509fe8f444a362ee416b288d7f44167328
niazsarbastahmad2002-dot <niazsarbastahmad2002@gmail.com>
2026-08-19 05:14:58 +0300

    Load Atlas interaction feedback styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ import "./atlas-brand-v2.css";
 import "./atlas-orbit.css";
 import "./atlas-reception-flow.css";
 import "./atlas-instant-flow.css";
+import "./atlas-interactions.css";
 import "./patient-clarity.css";
 
 export const metadata: Metadata = {
